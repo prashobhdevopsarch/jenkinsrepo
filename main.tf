180a7cc9d251aa83ad9c55722b279d6de50e9132
prashobhdevopsarch <62069646+prashobhdevopsarch@users.noreply.github.com>
2025-06-27 16:12:19 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "example" {
-  ami           = "ami-0c55b159cbfafe1f0"  # Use valid AMI for your region
+  ami           = "ami-0c803b171269e2d72"  # Use valid AMI for your region
   instance_type = "t2.micro"
 
   tags = {
